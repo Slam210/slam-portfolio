44a7712bc4d5db353c9255b79e8f06a75406bb9f
Steven Lam <stevenl.inbox@gmail.com>
2025-07-31 17:50:13 -0700

    Typo with type collection

--- src/app/projects/[uid]/page.tsx
+++ src/app/projects/[uid]/page.tsx
@@ -10,20 +10,18 @@ type ParamsType = Promise<{ uid: string }>;
 export default async function Page(props: { params: Promise<ParamsType> }) {
   const { uid } = await props.params;
   const client = createClient();
-  const page = await client.getByUID("page", uid).catch(() => notFound());
+  const page = await client.getByUID("projects", uid).catch(() => notFound());
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
 
-export async function generateMetadata(
-  props: {
-    params: Promise<{ uid: string }>;
-  }
-): Promise<Metadata> {
+export async function generateMetadata(props: {
+  params: Promise<{ uid: string }>;
+}): Promise<Metadata> {
   const params = await props.params;
   const client = createClient();
   const page = await client
-    .getByUID("page", params.uid)
+    .getByUID("projects", params.uid)
     .catch(() => notFound());
 
   return {
@@ -34,7 +32,7 @@ export async function generateMetadata(
 
 export async function generateStaticParams() {
   const client = createClient();
-  const pages = await client.getAllByType("page");
+  const pages = await client.getAllByType("projects");
 
   return pages.map((page) => ({ uid: page.uid }));
 }
